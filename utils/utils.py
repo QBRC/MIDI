8b40e1722e2a88f2529f860636cc7d34ca804452
Tingyi Wanyan <tiwanyan@gmail.com>
2025-03-24 11:38:52 -0500

    correction

--- utils/utils.py
+++ utils/utils.py
@@ -187,9 +187,7 @@ def extract_input_data_midi(batch_drug_name, batch_smile_seq, batch_cell_line_na
     
 	for smile_seq_origin in batch_smile_seq:
 		interpret_smile = list(generate_interpret_smile(smile_seq_origin)[0])
-		print(interpret_smile)
 		input_drug_atom_names = tf.constant(interpret_smile)
-		print(input_drug_atom_names)
 		input_drug_atom_index = string_lookup(input_drug_atom_names)-1
 		input_drug_atom_one_hot = layer_one_hot(input_drug_atom_index)
 		shape_drug_miss = input_drug_atom_one_hot.shape[0]
